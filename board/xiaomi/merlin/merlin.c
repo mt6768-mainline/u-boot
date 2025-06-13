1eba85c757b9f5ec1176b9d571275a75b972d7fe
catfish03 <catfish03@tutanota.com>
2025-06-13 05:01:07 +0300

    board: xiaomi: merlin: fix (or not) memory map

--- board/xiaomi/merlin/merlin.c
+++ board/xiaomi/merlin/merlin.c
@@ -8,6 +8,7 @@ DECLARE_GLOBAL_DATA_PTR;
 
 int board_init(void)
 {
+  gd->bd->bi_boot_params = CFG_SYS_SDRAM_BASE + 0x100;
   return 0;
 }
 
@@ -19,13 +20,22 @@ static struct mm_region merlin_mem_map[] = {
     .size = 0x100000000UL,
     .attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) | PTE_BLOCK_OUTER_SHARE,
   }, {
+    /* Peripherals */
+    .virt = 0x00000000UL,
+    .phys = 0x00000000UL,
+    .size = 0x40000000UL,
+    .attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) |
+       PTE_BLOCK_NON_SHARE | PTE_BLOCK_PXN | PTE_BLOCK_UXN
+  }, {
     /* Framebuffer */
     .virt = 0x7d9b0000UL,
     .phys = 0x7d9b0000UL,
     .size = 0x2250000UL,
-    .attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) | PTE_BLOCK_OUTER_SHARE,
+    .attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) | 
+        PTE_BLOCK_NON_SHARE | PTE_BLOCK_PXN | PTE_BLOCK_UXN
   }, {
     /* List terminator */
+    0,
   }
 };
 
